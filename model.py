da26ddef79c4e6bb789ba66cbe8a3db16491a455
vagrant <vagrant@vagrant.vm>
2016-11-03 20:49:08 +0000

    created model.py file, completed seed.py to extract all the data from csv file, will need to clean the code in seed.py a bit

---
+++ model.py
@@ -0,0 +1,160 @@
+"""Models and database functions for Best Book project."""
+import time
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class Book(db.Model):
+    """Book."""
+
+    __tablename__ = 'books'
+
+    book_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    description = db.Column(db.Text)
+    pages = db.Column(db.Integer, nullable=True)
+    published = db.Column(db.DateTime)
+    language = db.Column(db.String(15), nullable=True)
+    isbn10 = db.Column(db.Integer, nullable=True)
+    isbn13 = db.Column(db.Integer, nullable=True)
+
+    # Define relationship to genre, through books_genres
+    genres = db.relationship("Genre",
+                             secondary="books_genres",
+                             backref="books")
+
+    # Define relationship to author table, through books_authors
+    authors = db.relationship("Author",
+                             secondary="books_authors",
+                             backref="books")
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+        return "<Book id=%s title=%s>" % (self.book_id, self.title)
+
+class Award(db.Model):
+    """Book awards"""
+
+    __tablename__ = "awards"
+
+    award_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+        return "<Award id=%s name=%s>" % (self.award_id, self.name)
+
+class BookAward(db.Model):
+    """Awar of a specific book."""
+
+    __tablename__ = 'books_awards'
+
+    book_award_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    book_id = db.Column(db.Integer,
+                        db.ForeignKey('books.book_id'),
+                        nullable=False)
+    award_id = db.Column(db.Integer,
+                         db.ForeignKey('awards.award_id'),
+                         nullable=False)
+    year = db.Column(db.DateTime)
+
+    book = db.relationship("Book", backref="books_awards")
+    award = db.relationship("Award", backref="books_awards")
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<BookA book_id=%s award_id=%s year=%s>" % (self.book_id, self.genre_id, self.year)
+
+
+class Genre(db.Model):
+    """Genre of books"""
+
+    __tablename__ = "genres"
+
+    genre_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    genre = db.Column(db.String(30), nullable=True)
+
+
+class BookGenre(db.Model):
+    """Genre of a specific book."""
+
+    __tablename__ = 'books_genres'
+
+    book_genre_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    book_id = db.Column(db.Integer,
+                        db.ForeignKey('books.book_id'),
+                        nullable=False)
+    genre_id = db.Column(db.Integer,
+                         db.ForeignKey('genres.genre_id'),
+                         nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<BookGenre book_id=%s genre_id=%s>" % (self.book_id, self.genre_id)
+
+class Author(db.Model):
+    """Authors of books"""
+
+    __tablename__ = "authors"
+
+    author_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    # last_name = db.Column(db.String(30), nullable=False)
+    biography = db.Column(db.Text)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Author author_id=%s name=%s>" % (self.author_id, self.name)
+
+class BookAuthor(db.Model):
+    """Author of a specific book."""
+
+    __tablename__ = 'books_authors'
+
+    book_author_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    book_id = db.Column(db.Integer,
+                        db.ForeignKey('books.book_id'),
+                        nullable=False)
+    author_id = db.Column(db.Integer,
+                         db.ForeignKey('authors.author_id'),
+                         nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<BookAuthor book_id=%s author_id=%s>" % (self.book_id, self.author_id)
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bestbooks'
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ seed.py
@@ -0,0 +1,163 @@
+"""Utility file to seed bestbooks database from bestbooks file in data/"""
+import csv
+import datetime
+from sqlalchemy import func
+
+from model import Book, Award, BookAward, Genre, BookGenre, Author, BookAuthor, connect_to_db, db
+from server import app
+
+def load_books():
+    """Load books from bestbooks.csv into database."""
+
+    print "Book!"
+
+    # open the csv file and unpack it
+    with open("/home/vagrant/src/best_books/data/bestbooks.csv") as general:
+        reader = csv.reader(general)
+        #unpacking each row in the file and looping over it.
+        #appending each title to the titles list
+        for award, year, genre, title, author, author2, author3, genre_tag, award_tag in reader:
+
+            # The date is in the file as year string;
+            # we need to convert it to an actual datetime object.
+            year = datetime.datetime.strptime(year, "%Y")
+            author = author.strip()
+
+            #create book object
+            #first, we'll check if this current book title we already have in the book table
+            #if we don't, then we have to create a book object
+            #add it to session and commit it to the database
+            #using func.lower helps to compare data without case sensitivity
+            book = Book.query.filter(func.lower(Book.title) == func.lower(title)).first()
+            if not book:
+                book = Book(title=title)
+                db.session.add(book)
+                db.session.commit()
+
+            #create award object
+            book_award = Award.query.filter(func.lower(Award.name) == func.lower(award)).first()
+            if not book_award:
+                book_award = Award(name=award)
+                db.session.add(book_award)
+                db.session.commit()
+
+            #create book award object
+            get_book_award = BookAward.query.filter(BookAward.year == year,
+                                                    BookAward.book_id == book.book_id,
+                                                    BookAward.award_id == book_award.award_id).first()
+            if not get_book_award:
+                books_awards = BookAward(book_id=book.book_id,
+                                          award_id=book_award.award_id,
+                                          year=year)
+                db.session.add(books_awards)
+                db.session.commit()
+
+            #create genre object
+            if genre:
+                get_genre = Genre.query.filter(func.lower(Genre.genre) == func.lower(genre)).first()
+                if not get_genre:
+                    new_genre = Genre(genre=genre)
+                    db.session.add(new_genre)
+                    db.session.commit()
+
+            #create book genre object
+            get_book_genre = BookGenre.query.filter(BookGenre.book_id == book.book_id, 
+                                                    BookGenre.genre_id == new_genre.genre_id).first()
+            if not get_book_genre:
+                books_genres = BookGenre(book_id=book.book_id,
+                                          genre_id=new_genre.genre_id)
+                db.session.add(books_genres)
+                db.session.commit()
+
+            #create first author object
+            author1 = Author.query.filter(func.lower(Author.name) == func.lower(author)).first()
+            if not author1:
+                new_author = Author(name=author)
+                db.session.add(new_author)
+                db.session.commit()
+
+            #create book author object for the first author
+            get_book_author = BookAuthor.query.filter(BookAuthor.book_id == book.book_id,
+                                                    BookAuthor.author_id == new_author.author_id).first()
+            if not get_book_author:
+                books_authors = BookAuthor(book_id=book.book_id,
+                                          author_id=new_author.author_id)
+                db.session.add(books_authors)
+                db.session.commit()
+
+
+            # need to check if the book has a second author
+            # if it does then we will check if this author is in the database
+            # if it doesn't then we'll create a new author object,
+            # add it to session and commit to the database.
+            if author2:
+                get_author2 = Author.query.filter(func.lower(Author.name) == func.lower(author2)).first()
+                if not get_author2:
+                    new_author2 = Author(name=author2)
+                    db.session.add(new_author2)
+                    db.session.commit()
+
+                    # once we added this author to our database author table
+                    # we can create a books author connection object to the books authors table
+                    books_authors = BookAuthor(book_id=book.book_id,
+                                                author_id=new_author2.author_id)
+
+                # if we have this author in our database authors table, then
+                # we have to check if we have this author book assossiation in our
+                # books authors table.
+                # if we don't, then we'll create this assossiation object in the
+                # books authors table
+                else:
+                    get_book_author2 = BookAuthor.query.filter(BookAuthor.book_id == book.book_id,
+                                                        BookAuthor.author_id == get_author2.author_id).first()
+                    if not get_book_author2:
+                        books_authors = BookAuthor(book_id=book.book_id,
+                                                  author_id=get_author2.author_id)
+                db.session.add(books_authors)
+                db.session.commit()
+
+            # need to check if the book has a third author
+            # if it does then we will check if this author is in the database
+            # if it doesn't then we'll create a new author object,
+            # add it to session and commit to the database
+            if author3:
+                get_author3 = Author.query.filter(func.lower(Author.name) == func.lower(author3)).first()
+                if not get_author3:
+                    new_author3 = Author(name=author3)
+                    db.session.add(new_author3)
+                    db.session.commit()
+
+                    # once we added this author to our database author table
+                    # we can create a books author connection object to the books authors table
+                    books_authors = BookAuthor(book_id=book.book_id,
+                                                author_id=new_author3.author_id)
+
+                # if we have this author in our database authors table, then
+                # we have to check if we have this author book assossiation in our
+                # books authors table.
+                # if we don't, then we'll create this assossiation object in the
+                # books authors table
+                else:
+                    get_book_author3 = BookAuthor.query.filter(BookAuthor.book_id == book.book_id,
+                                                        BookAuthor.author_id == get_author3.author_id).first()
+                    if not get_book_author3:
+                        books_authors = BookAuthor(book_id=book.book_id,
+                                                  author_id=get_author3.author_id)
+                db.session.add(books_authors)
+                db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    db.create_all()
+
+    BookAward.query.delete()
+    BookGenre.query.delete()
+    BookAuthor.query.delete()
+    Book.query.delete()
+    Award.query.delete()
+    Genre.query.delete()
+    Author.query.delete()
+
+
+    load_books()
